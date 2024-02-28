e677b2b077a467cdba6c6eadc7402a7e9a5cc5cf
Team7034 <wlhsfrc@gmail.com>
2024-02-27 20:27:11 -0800

    Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -64,7 +64,7 @@ public final class Constants {
 		public static final int lowerNeoID = 13;
 		public static final int upperNeoID = 12;
 		public static final double lowerIntakeSpeed = 0.5;
-		public static final double upperIntakeSpeed = 0.7;
+		public static final double upperIntakeSpeed = 0.5;
 	}
 	public static final class Climb {
 		public static final int upperFalconID = 9;
