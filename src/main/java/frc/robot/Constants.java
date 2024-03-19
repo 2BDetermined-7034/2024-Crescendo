4b0b91736521ccbecd87c421bf76b83caa31b3f0
Team7034 <wlhsfrc@gmail.com>
2024-03-18 17:11:21 -0700

    shooter motion profiling

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -85,8 +85,8 @@ public final class Constants {
 		public static final double angleGearRatio = 1d / 12.72d;
 		public static final double angleFrontHardstop = -60;
 		public static final double angleBackHardstop = 63.0;
-		public static final PIDConstants anglePIDConstants = new PIDConstants(1,0,0.2);
-		public static final double angleFeedForward = 0.4;
+		public static final PIDConstants anglePIDConstants = new PIDConstants(1,2.5,0);
+		public static final double angleFeedForward = 0.6;
 
 	}
 	public static final class Intake {
