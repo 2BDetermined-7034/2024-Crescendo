453fe756c05c8793a4ed4004625de0b556ab2cb0
Cardin <nekk.ng3@gmail.com>
2024-03-26 18:57:07 -0700

    Edited autos and working on amp two piece code auto

--- src/main/java/frc/robot/commands/Auto/AutoFactory.java
+++ src/main/java/frc/robot/commands/Auto/AutoFactory.java
@@ -3,7 +3,13 @@ package frc.robot.commands.Auto;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathPlannerPath;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.sensors.LaserCANSensor;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
 
 public class AutoFactory {
 
@@ -18,5 +24,75 @@ public class AutoFactory {
 		return new PathPlannerAuto(pathName);
 	}
 
+
+	public static Command constantShooter(ShooterSubsystem shooter) {
+		return Commands.startEnd(
+				() -> shooter.setLaunchTalon(80),
+				() -> shooter.setLaunchTalon(0),
+				shooter
+		);
+	}
+
+	public static Command stallIntake(IntakeSubsystem intake, ShooterSubsystem shooter, LaserCANSensor intakeLaser, LaserCANSensor shooterLaser) {
+		return new FunctionalCommand(
+				() -> {},
+				() -> {
+
+					if(shooterLaser.getLatestMeasurement() < 50) {
+						shooter.setNeoSpeeds(0);
+					} else {
+						shooter.setNeoSpeeds(0.05);
+						intake.run(Constants.Intake.lowerIntakeSpeed, Constants.Intake.upperIntakeSpeed);
+					}
+
+				},
+				(interrupted) -> {
+					shooter.setNeoSpeeds(0);
+					intake.run(0,0);
+				},
+				() -> false
+		);
+	}
+
+	public static Command shootNote(ShooterSubsystem shooter, LaserCANSensor shooterLazer) {
+		return new FunctionalCommand(
+				() -> {},
+				() -> {
+					if(shooterLazer.getLatestMeasurement() < 50) {
+						shooter.setNeoSpeeds(0.05);
+					} else {
+						shooter.setNeoSpeeds(0);
+					}
+				},
+				(interrupted) -> {
+					shooter.setNeoSpeeds(0);
+				},
+				() -> false
+		);
+	}
+
+	public static Command angleShooter(ShooterSubsystem shooter, double angle) {
+		return Commands.startEnd(
+				() -> shooter.setAngleTalonPositionDegrees(angle),
+				() -> shooter.setAngleTalonPositionDegrees(0),
+				shooter
+		);
+	}
+
+	public static Command ampTwoPiece() {
+		return new ParallelCommandGroup(
+				constantShooter(RobotContainer.shooterSubsystem),
+				new SequentialCommandGroup(
+						new ParallelRaceGroup(
+								new WaitCommand(1),
+								shootNote(RobotContainer.shooterSubsystem, RobotContainer.shooterLaser)
+						),
+						new ParallelRaceGroup(
+								followPath("Amp to W1"),
+								stallIntake(RobotContainer.intakeSubsystem, RobotContainer.shooterSubsystem, RobotContainer.intakeLaser, RobotContainer.shooterLaser)
+						)
+				)
+				);
+	}
 
 }
